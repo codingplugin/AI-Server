840c3793cdacad66d5cbb01653fd4ccc94f67476
Subhradip Nandi <your-email@example.com>
2025-07-13 21:42:04 +0530

    Remove all non-server files; keep only backend for deployment

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    // If you use version catalogs, keep this line:
-    alias(libs.plugins.android.application) apply false
-    // Do NOT add id("org.jetbrains.kotlin.android") here!
-}
